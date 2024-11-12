618c7d466fde2ded26de9d8b9e50853679c38fc2
ajityaduv <100124329+ajityaduv@users.noreply.github.com>
2024-11-12 17:41:02 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 123457
+let accountEmail = "ajit@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "ajit@html.com"
+accountPassword = "54321"
+accountCity = "Noida"
+
+// console.log(accountId);
+
+
+/*
+Prefer not to use 
+because of issue in block sccope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all Js code as newer version
+
+// alert(3+3) we are using nodeja and not a browser 
+// alert("hello")
+// code readability should be high
+
+console.log(3+3);
+console.log("Ajit")
+
+
+// number =>2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value  representation of empty value
+// undefined => variable declared but not assigned any value
+// symbol => unique
+
+
+
+// object 
+
+
+console.log(typeof null);
