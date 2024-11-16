5d3ab6e07e9d199d5792311b044e69aef60eafb9
ajityaduv <100124329+ajityaduv@users.noreply.github.com>
2024-11-16 07:41:10 +0000

    control

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+// conditionals
+const isUserLoggedIn = true
+const temperature = 41
+
+// if( temperature == 50){
+//     console.log("Less than 50")
+// }
+// else{
+//     console.log("Temperature is greater than 50");
+// }
+
+// <, >, <=, >=, ==, === (Checks the data type also), !==
+
+// const score = 200
+// if (score >100){
+//     let power = "Fly"
+//     console.log(`User Power is: ${power}`);
+// }
+
+// console.log(`User Power ${power}`)
+
+
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2");   
+// not a good syntactical practice
+
