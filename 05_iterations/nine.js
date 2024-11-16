d79ad59cf701bbf7e3e6e961534c1447d32a1c15
ajityaduv <100124329+ajityaduv@users.noreply.github.com>
2024-11-16 21:05:41 +0000

    partone done

---
+++ 05_iterations/nine.js
@@ -0,0 +1,30 @@
+const myNums = [1, 2, 3, 4, 5];
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`)
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, currval) => (acc+currval) , 0)
+
+// console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "Python course",
+        price: 999
+    },
+    {
+        itemName: "Data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) =>item.price+acc,0)
+
+console.log(priceToPay);
